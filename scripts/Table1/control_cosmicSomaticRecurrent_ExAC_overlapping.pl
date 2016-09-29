55da1e7febc2fb7edada28c17168d4ee0af00c51
CosmicHGMD <mengma2@gmail.com>
2016-09-29 11:19:35 -0500

    Add files via upload

---
+++ scripts/Table1/control_cosmicSomaticRecurrent_ExAC_overlapping.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+
+
+open control, "ExAC_variants.txt";
+
+my $no = 153593;
+
+my %control;
+my $cur=0;
+while (my $line = <control>) {
+    $line =~ s/\s+$//;
+    my @a = split /\t/, $line;
+    $control{$cur++} = $a[0].$a[1].$a[3].$a[4];     
+    if ($cur % 100000 == 0){
+    	  print $cur."\n";
+    	}
+}
+print "\n";
+my $ctrsum = $cur;
+
+open cosmic, "cosmicv73RecurrentSomatic.txt";
+my %cosmic;
+my $cosno = 0;
+while (my $line = <cosmic>) {    
+    $line =~ s/\s+$//g;
+    my @a = split /\t/, $line; 
+    $cosmic{$a[0].$a[1].$a[3].$a[4]} = 1;    #chr.start.ref.end
+}
+
+open output, ">Result_controlExAC_cosmicRecurrentSomatic_1000";
+print output "NO\tOverlapping\n";
+for (my $k=0; $k<1000; $k++){
+    
+    my %ctr;
+    for ($ctrno=0; $ctrno<$no; $ctrno++){
+        my $ctrcur = int(rand($ctrsum));
+        if ( $ctr{$control{$ctrcur}} == 1 ) {
+             $ctrno--; 
+            }
+        else {
+            $ctr{$control{$ctrcur}} = 1;            
+        }        
+    }
+    my $overlapping=0;
+    foreach $key(%ctr){
+    	if ($cosmic{$key} == 1){
+    		$overlapping++;    		 
+    		#print $key."\n";  
+    		}    	
+    }
+    print output  $k."\t".$overlapping."\n";        
+    print $k."\t".$overlapping."\n";
+}
+
+
+
+
